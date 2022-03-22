25336909cf8ab385387bb42430c480f4b8ddfc66
Quentin-bp <73823634+Quentin-bp@users.noreply.github.com>
2022-03-22 09:44:54 +0100

    Rajout début projet

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <NavBar :titles="titles" />
+    <keep-alive>
+    <router-view name="locCentral" :collec="collec" @store-virus="collec.push($event)"></router-view>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+  import NavBar from './components/NavBar.vue'
+  import {viruses} from './model.js'
+
+  export default {
+    name: 'App',
+    data : () => {
+      return {
+        titles : [ { text: "Home", color:"black", path:"/home"}, { text: "Lab", color:"blue", path:"/labo/slice"}, { text: "Library", color:"red", path:"/library/view"} ],
+        currentMenu : 0,
+        collec : viruses
+      }
+    },
+    components: {
+      NavBar
+    },
+  }
+</script>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    samples : [],
+    parts : [],
+  },
+  mutations: {
+    receiveVirus : function(state,viruses) {
+      viruses.forEach(v => state.samples.push(v));
+      }
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
