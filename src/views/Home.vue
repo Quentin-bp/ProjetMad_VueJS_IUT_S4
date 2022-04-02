dcd0516c968ebd7eecfca6716402412b2f9c8cc0
Quentin-bp <73823634+Quentin-bp@users.noreply.github.com>
2022-04-02 03:44:57 +0200

    améliorations visuelles et rajout 1ere partie de librairie

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,39 +1,40 @@
 <template>
-  <v-parallax  :src="require('../assets/devanture.png')"  >
-
-    <v-card max-width="800" class="mx-auto my-auto" outlined color="rgb(0, 0, 0, 0.5)">
-      <v-card-title class="justify-center ma-0 white--text text-h2">
-        <div>
-
-          <h3 class="headline pink--text text--accent-2">
-            <v-icon large>mdi-home</v-icon>
-            Entrée
-          </h3>
-        </div>
-      </v-card-title>
-      <v-card-text class="font-weight-bold text-h5 text-center">
-          Bienvenue dans le laboratoire abandonné d'Artemis, la déesse qui peut
-          engendrer des épidémies
-          Si tu veux récuperer son travail et analyser les virus déjà découvert,
-          regarde dans la librairie
-          Pour te tenir sur un pied d'égalité avec elle, on te donne également
-          le pouvoir de créer tes propres virus, vas voir dans le laboratoire !
-      </v-card-text>
-    </v-card>
-  </v-parallax>
+  <v-card class="justify-content" style="align-item: center">
+    <v-img :src="require('../assets/devanture.png')" class="justify-center">
+    <v-container style="display:grid; justify-content:center">
+        <v-card-title class="justify-center ma-0 white--text text-h2">
+          <div>
+            <h3 class="headline pink--text text--accent-2 font-weight-bold text-center" style="background-color:#1E1E1E; opacity:0.7; width : 200px">
+              <v-icon large>mdi-home</v-icon>
+              Entrée
+            </h3><br>
+            </div>
+        </v-card-title>
+        <v-card class=" text-h2" style="max-width: 800px; opacity : 0.9">
+          <v-card-text class="font-weight-bold text-h5 text-center justify-center">
+            Bienvenue dans le laboratoire abandonné d'Artemis, la déesse qui
+            peut engendrer des épidémies. Si tu veux récuperer son travail et
+            analyser les virus déjà découvert, regarde dans la librairie. Pour
+            te tenir sur un pied d'égalité avec elle, on te donne également le
+            pouvoir de créer tes propres virus, vas voir dans le laboratoire !
+          </v-card-text>
+        </v-card>
+    </v-container>
+    </v-img>
+  </v-card>
 </template>
 
 <script>
 export default {
   name: "Home",
 
-  computed : {
-    devanture(){
-      console.log("HEY" + this.$store.state.devanture)
-      return{
-        Devanture: require(this.$store.state.devanture)
-      }
-    }
-  }
+  computed: {
+    devanture() {
+      console.log("HEY" + this.$store.state.devanture);
+      return {
+        Devanture: require(this.$store.state.devanture),
+      };
+    },
+  },
 };
 </script>
--- src/views/Library.vue
+++ src/views/Library.vue
@@ -1,20 +1,40 @@
 <template>
-  <v-card>
-      <v-card-title>Library</v-card-title>
-      <Articles></Articles>
+  <v-card outlined color="rgb(0, 0, 0, 0.5)">
+    <v-img :src="require('../assets/librairie.gif')">
+      <v-container style="display: grid; justify-content: center">
+        <v-card-title class="justify-center ma-0 white--text text-h2">
+          <div>
+            <h3 class=" headline pink--text text--accent-2 font-weight-bold text-center"  style="background-color: #1e1e1e; opacity: 0.7; width: 200px"  >
+              Library
+            </h3>
+            <br />
+          </div>
+        </v-card-title>
+        <v-card class="text-h2" style="max-width: 800px; opacity: 0.9">
+          <v-card-text class="font-weight-bold text-h5 text-center justify-center" >
+            <Articles />
+            </v-card-text>
+        </v-card>
+        
+        </v-container>
+      <v-container style="display: grid; justify-content: center">
+        <v-card style="max-width: 800px; opacity: 0.9">
       <router-view name="locDown"></router-view>
+         </v-card>
+      </v-container>
+      
+    </v-img>
   </v-card>
 </template>
 
 <script>
-
-  import Articles from '../components/Articles.vue'
-  export default {
-    name: 'Library',
-    components: {
-      Articles
-    }
-  }
+import Articles from "../components/Articles.vue";
+export default {
+  name: "Library",
+  components: {
+    Articles,
+  },
+};
 </script>
 
 <style scoped>
